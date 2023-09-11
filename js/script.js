d98b230992eb0dcf9330af3c537314870de08a6c
R-G2110 <argeldelacruz86@gmail.com>
2023-09-11 15:47:09 +0200

    [Add]: steps 1, 2, 3, 4 on script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 /*PASSAGGI:
 1. stampare i numeri da 1 a 100.
-2. scegliere i multipli di 3 e stampare 'Fizz' 
-3. scegliere i multipli di 5 e stampare 'Buzz'
-4. scegliere i multipli siadi 3 sia di 5 e stampare 'FizzBuzz'
+2. scegliere i multipli sia di 3 che di 5 e stampare 'FizzBuzz'
+3. scegliere i multipli di 3 e stampare 'Fizz' 
+4. scegliere i multipli di 5 e stampare 'Buzz'
 */ 
+
+
+// 1.stampare i numeri da 1 a 100.
+const boxesContainer = document.querySelector('.boxes');
+for(let i = 1; i <= 100; i++){
+  const box = document.createElement('div');
+  box.classList.add('box');
+  
+ //2. scegliere i multipli sia di 3 che di 5 e stampare 'FizzBuzz'
+	if(!(i % 3) && !(i % 5)){
+    box.classList.add('fizzbuzz');
+		box.append('FizzBuzz');
+		//3.scegliere i multipli di 3 e stampare 'Fizz'
+  } else  if(!(i  % 3)){
+		box.classList.add('fizz');
+		box.append('Fizz');
+		//4. scegliere i multipli di 5 e stampare 'Buzz'
+  } else if(!(i % 5)){
+    box.classList.add('buzz');
+		box.append('Buzz');
+  } else {
+		box.append(i);
+	}
+  
+  boxesContainer.append(box);
+}
