6e274d89805cbc4b846a805a94253230f8613016
R-G2110 <argeldelacruz86@gmail.com>
2023-09-11 15:59:24 +0200

    [Add]: circle transfomation on hover

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 3. scegliere i multipli di 3 e stampare 'Fizz' 
 4. scegliere i multipli di 5 e stampare 'Buzz'
 */ 
+const fizz = 3;
+const buzz = 5;
 
 
 // 1.stampare i numeri da 1 a 100.
@@ -13,15 +15,15 @@ for(let i = 1; i <= 100; i++){
   box.classList.add('box');
   
  //2. scegliere i multipli sia di 3 che di 5 e stampare 'FizzBuzz'
-	if(!(i % 3) && !(i % 5)){
+	if(!(i % fizz) && !(i % buzz)){
     box.classList.add('fizzbuzz');
 		box.append('FizzBuzz');
 		//3.scegliere i multipli di 3 e stampare 'Fizz'
-  } else  if(!(i  % 3)){
+  } else  if(!(i  % fizz)){
 		box.classList.add('fizz');
 		box.append('Fizz');
 		//4. scegliere i multipli di 5 e stampare 'Buzz'
-  } else if(!(i % 5)){
+  } else if(!(i % buzz)){
     box.classList.add('buzz');
 		box.append('Buzz');
   } else {
